13eb16964516fd89b2f4cab2ca9bd176f390782d
Paree Bachate <pareebachate20@yahoo.com>
2026-08-21 10:22:55 +0530

    APP Assignment 4.py

--- APP Assignment 4.py
+++ APP Assignment 4.py
@@ -1,12 +1,25 @@
-memo={} #creates a dictionary to store previously calculated fibonacci series, key is n and value is its summed series
-def fib(n):
-    if n<=1:
-        return n #stops recursion when n is less than one
-    if n in memo:
-        return memo[n] #if series already present in dictionary, prints that (memoization)
-    memo[n]=fib(n-1)+fib(n-2) #uses fibonacci series of smaller numbers to calculate series of larger number (recursion)
-    return memo[n]
+import time
+from functools import lru_cache
 
-while True: #keeps running loop forever
-    num=int(input("Enter Number: "))
-    print("Fibonacci Series of",num,":",fib(num))
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+start = time.time()
+print(fib(35))
+end = time.time()
+
+print("Without Using lru_cache: Execution time:", end - start, "seconds")
+
+@lru_cache(maxsize=None)
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+start = time.time()
+print(fib(35))
+end = time.time()
+
+print("With Using lru_cache: Execution time:", end - start, "seconds")
