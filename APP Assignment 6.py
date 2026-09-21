a4f3beff1e6679260b7b263979638b6649d914c9
Paree Bachate <pareebachate20@yahoo.com>
2026-09-21 09:52:30 +0530

    Add files via upload

---
+++ APP Assignment 6.py
@@ -0,0 +1,50 @@
+def knapsack_bottom_up(values, weights, W):
+    n = len(values)
+
+    dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]              #create DP Table
+
+    for i in range(1, n + 1):
+        for w in range(W + 1):
+
+            if weights[i - 1] <= w:                                     #if weight of item is less than current capacity
+                dp[i][w] = max(
+                    dp[i - 1][w],                                       #exclude current item
+                    dp[i - 1][w - weights[i - 1]] + values[i - 1]       #include current item
+                )
+            else:
+                dp[i][w] = dp[i - 1][w]         #exclude the item as it exceeds current weight
+
+    return dp[n][W]             #return maximum obtainable value
+
+
+def knapsack_top_down(values, weights, W):
+    n = len(values)
+    memo = [[-1 for _ in range(W + 1)] for _ in range(n + 1)]           #creates a table filled with -1 meaning we havent calculated best value yet 
+
+    def solve(i, w):
+        if i == 0 or w == 0:
+            return 0
+
+        if memo[i][w] != -1:                            #checks if previously calculated
+            return memo[i][w]
+
+        if weights[i - 1] <= w:
+            memo[i][w] = max(
+                solve(i - 1, w),
+                solve(i - 1, w - weights[i - 1]) + values[i - 1]
+            )
+        else:
+            memo[i][w] = solve(i - 1, w)
+
+        return memo[i][w]                       # Start with all items and full capacity
+
+    return solve(n, W)
+
+
+# Example Input
+values = [60, 100, 120]
+weights = [10, 20, 30]
+W = 50
+
+print("Bottom-Up Approach:", knapsack_bottom_up(values, weights, W))
+print("Top-Down Approach:", knapsack_top_down(values, weights, W))
